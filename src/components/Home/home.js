b259e8f397c7d5b83328f0efef72b7539b4bf527
AlexDosy <66622992+AlexDosy@users.noreply.github.com>
2025-11-09 15:20:36 +0000

    home update

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -5,17 +5,38 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Hi, I'm Alex D V</h1>
-      <p>Web Developer | Designer | Problem Solver</p>
-      <button onClick={() => window.scrollTo(0, document.body.scrollHeight)}>
-        Contact Me
-      </button>
-      <Link to="/three-scene">
-        <button >Check out this cool 3D cube below!</button>
-      </Link>
-          </div>
+      {/* animated background layer */}
+      <div className="gradient-bg" />
+
+      {/* decorative floating blobs */}
+      <div className="blobs" aria-hidden="true">
+        <span className="blob b1" />
+        <span className="blob b2" />
+        <span className="blob b3" />
+      </div>
+
+      <div className="home-content">
+        <h1 className="fadeIn">Hi, I'm Alex D V</h1>
+        <p className="fadeIn delay-1">Web Developer | Designer | Problem Solver</p>
+
+        <div className="actions fadeIn delay-2">
+          <button
+            className="btn contact"
+            onClick={() =>
+              window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+            }
+            aria-label="Scroll to contact"
+          >
+            Contact Me
+          </button>
+
+          <Link to="/three-scene" aria-label="Go to 3D scene">
+            <button className="btn secondary">Check out this cool 3D cube below!</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
-
 
 export default Home;
